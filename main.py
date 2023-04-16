6f30107ef92e0135a63764ed0290013760b6d7a7
Beknasar <680633@gmail.com>
2023-04-16 17:01:45 +0300

    Created Delete_post route, updated templates.

--- main.py
+++ main.py
@@ -50,9 +50,9 @@ def get_all_posts():
     return render_template("index.html", all_posts=posts)
 
 
-@app.route("/post/<int:index>")
-def show_post(index):
-    requested_post = db.session.query(BlogPost).get(index)
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    requested_post = db.session.query(BlogPost).get(post_id)
     return render_template("post.html", post=requested_post)
 
 
@@ -110,8 +110,16 @@ def edit_post(post_id):
     #     edit_form.populate_obj(post)
     #     db.session.commit()
 
-        return redirect(url_for('show_post', index=post.id))
+        return redirect(url_for('show_post', post_id=post.id))
     return render_template('make-post.html', form=edit_form, id=post_id)
+
+
+@app.route("/delete/<int:post_id>")
+def delete_post(post_id):
+    post = db.session.query(BlogPost).get(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('get_all_posts'))
 
 
 if __name__ == "__main__":
